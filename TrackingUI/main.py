d03a72af2f2f189d20c13938c56f102ebbc167ce
Miles Calloway <26465212+mcallbosco@users.noreply.github.com>
2024-11-26 20:11:57 -0500

    sync

--- TrackingUI/main.py
+++ TrackingUI/main.py
@@ -1,22 +1,23 @@
 import flet as ft
 from flet import TemplateRoute
 
-
+showExampleStuff = True
 #tracking number specification
 #first 10 digits are the package number
 #Last 2 digits are the previous numbers added together
 #The last 2 numbers are not stored in the database, they are only used to verify the number before checking serverside. 
 
 def main(page: ft.Page):
+    
     page.title = "Flet counter example"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    txt_number = ft.TextField(text_align=ft.TextAlign.RIGHT, width=100)
     
 
     def checkTracking(number):
         if len(number) == 12:
-            if number[:-2] == str(sum([int(x) for x in number[:-2]])):
+            if number[10:12] == str(sum([int(x) for x in number[:-2]])):
                 return True
         return False
     
@@ -32,8 +33,23 @@ def main(page: ft.Page):
         troute = TemplateRoute(page.route)
         
 
+        
+
     def checkCallback(e):
+        doInitialFormatCheck = True
+
+        if doInitialFormatCheck:
+            if not checkTracking(txti_tracking.value):
+                txti_tracking.border_color = ft.colors.RED
+                errorMessage.visible = True
+                page.update()
+                return
+            
         page.go(txt_number.value)
+        page.update()
+        #set page title
+        page.title = "Tracking Number: " + txti_tracking.value
+        page.update()
 
     
 
@@ -42,14 +58,18 @@ def main(page: ft.Page):
     txti_tracking.width = page.width * 0.8
     but_check = ft.FilledTonalButton("Track")
     but_check.on_click = checkCallback
+    errorMessage = ft.Text("Invalid tracking number", color=ft.colors.RED, visible=False)
+    if showExampleStuff:
+        text = ft.Text("Tracking number: 123456789045")
+        page.add(text)
     
-    searchBarView = ft.Row(
+    searchBarView = ft.Column([ft.Row(
         [
             txti_tracking,
             but_check
         ],
         alignment=ft.MainAxisAlignment.CENTER
-    )
+    ),ft.Row([errorMessage, ft.Text("")])])
     page.add(searchBarView)
     page.on_route_change = route_change
     page.go(page.route)
